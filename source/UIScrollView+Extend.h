18879cbd07b6dab899182fb1f65bba2be7c50d57
brivio <brivio@qq.com>
2015-12-19 19:46:18 +0800

    代码更新

--- source/UIScrollView+Extend.h
+++ source/UIScrollView+Extend.h
@@ -1,5 +1,9 @@
 #import <Foundation/Foundation.h>
 
 @interface UIScrollView (Extend)
+- (void)setContentHeight:(CGFloat)height;
+
+- (void)setContentWidth:(CGFloat)width;
+
 - (void)goTop;
 @end
--- source/UIScrollView+Extend.m
+++ source/UIScrollView+Extend.m
@@ -2,6 +2,16 @@
 
 
 @implementation UIScrollView (Extend)
+- (void)setContentHeight:(CGFloat)height {
+    CGSize size = self.contentSize;
+    self.contentSize = CGSizeMake(size.width, height);
+}
+
+- (void)setContentWidth:(CGFloat)width {
+    CGSize size = self.contentSize;
+    self.contentSize = CGSizeMake(width, size.height);
+}
+
 - (void)goTop {
     self.contentOffset = CGPointMake(0, 0);
 }
